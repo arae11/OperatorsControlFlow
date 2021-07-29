2a06345e9f6b19b4bad95b1659c7c73da0bd1832
Alex Rae <arae@spartaglobal.com>
2021-07-29 11:19:24 +0100

    Added Loops

---
+++ OperatorsApp/LoopTypes.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsApp
+{
+    public static class LoopTypes
+    {
+        internal static int HighestForEachLoop(List<int> nums)
+        {
+            int highest = Int32.MinValue;
+            foreach (var n in nums)
+            {
+                if (n > highest)
+                {
+                    highest = n;
+                }
+            }
+            return highest;
+        }
+        internal static int HighestForLoop(List<int> nums)
+        {
+            int highest = 0;
+
+            for (int i = 0; i < nums.Count; i++)
+            {
+                if (nums[i] > highest)
+                {
+                    highest = nums[i];
+                }
+            }
+            return highest;
+        }
+        internal static int HighestWhileLoop(List<int> nums)
+        {
+            int highest = 0;
+            int i = 0;
+
+            while (i < nums.Count)
+            {
+                if (nums[i] > highest)
+                {
+                    highest = nums[i];
+                }
+                i++;
+            }
+            return highest;
+        }
+        internal static int HighestDoWhileLoop(List<int> nums)
+        {
+            int highest = 0;
+            int count = 0;
+
+            do
+            {
+                if (nums[count] > highest)
+                {
+                    highest = nums[count];
+                }
+                count++;
+            }
+            while (count < nums.Count);
+            return highest;
+        }
+    }
+}
--- OperatorsApp/Program.cs
+++ OperatorsApp/Program.cs
@@ -6,6 +6,13 @@ namespace OperatorsApp
     {
         static void Main(string[] args)
         {
+            // List<int> nums = new List<int> { 10, 6, 22, -17, 3 };
+            List<int> nums = new List<int> { -10, -6, -22, -17, -3 };
+            Console.WriteLine("Highest foreach loop: " + LoopTypes.HighestForEachLoop(nums));
+            Console.WriteLine("Highest for loop: " + LoopTypes.HighestForLoop(nums));
+            Console.WriteLine("Highest while loop: " + LoopTypes.HighestWhileLoop(nums));
+            Console.WriteLine("Highest do-while loop: " + LoopTypes.HighestDoWhileLoop(nums));
+
             //int a = 5 % 2;
 
             //var daysInAWeek = 7;
@@ -60,28 +67,30 @@ namespace OperatorsApp
             //    return num % 2 == 0;
             //}
 
-            
+
         }
-        public static string Priority(int level)
-        {
-            string priority = "Code ";
-            switch (level)
-            {
-                case 3:
-                    priority = priority + "Red";
-                    break;
-                case 2:
-                case 1:
-                    priority = priority + "Yellow";
-                    break;
-                case 0:
-                    priority = priority + "Green";
-                    break;
-                default:
-                    priority = " Error";
-                    break;
-            }
-            return priority;
-        }
+        //public static string Priority(int level)
+        //{
+        //    string priority = "Code ";
+        //    switch (level)
+        //    {
+        //        case 3:
+        //            priority = priority + "Red";
+        //            break;
+        //        case 2:
+        //        case 1:
+        //            priority = priority + "Yellow";
+        //            break;
+        //        case 0:
+        //            priority = priority + "Green";
+        //            break;
+        //        default:
+        //            priority = " Error";
+        //            break;
+        //    }
+        //    return priority;
+        //}
+
+        
     }
 }
